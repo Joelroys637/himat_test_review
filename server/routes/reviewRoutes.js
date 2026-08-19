c003a7c25f1f70a0eb9c29e1a58db1d0145980a6
LEO JOEL ROYS <joelroys637@gmail.com>
2026-08-19 06:38:23 +0530

    fin

--- server/routes/reviewRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const reviewController = require('../controllers/reviewController');
-
-router.post('/generate', reviewController.generateReview);
-
-module.exports = router;
--- server/routes/vendorRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const vendorController = require('../controllers/vendorController');
-const auth = require('../middleware/auth');
-
-router.put('/update', auth, vendorController.updateVendor);
-router.get('/:vendorId', vendorController.getVendor);
-
-module.exports = router;
